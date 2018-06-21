c7b8ef01a1bbd27fa1c2722cec10391b2c905d98
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-06-20 20:52:51 -0700

    second commit

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,100 +1,101 @@
+// Initial array of GIF Reactions
+let gifReactions = ["Eye Roll", "Face Palm", "Finger Guns", "Oh No You Didn't", "Slow Clap", "SMH",];
 
- 
-           // Initial array of emojis
-           let topics = ['happy','sad', 'angry'];
+// Function for displaying GIF data
+function renderButtons () {
+  // Deleting/emptyingthe GIFs prior to adding new GIFs
+  $("buttons-view").empty();
+  // Looping through the array of GIFs
+  for (let i =0; i < gifReactions.length; i++){
+  // Now dynamically generating buttons for each GIF in the array
+  let b = $("<button>");
+  // Adding a class of gif-btn to our button
+  b.addClass("gif-btn");
+  // Adding a data-attribute
+  b.attr("data-name", gifReactions[i]);
+  // Providing the initial button text
+  b.text(gifReactions[i]);
+  // Adding the button to the buttons-view div
+  $("#buttons-view").append(b);
+  
+  }
+  
+  }
 
-///////////////////////////////////////////////////////////////
 
-        // Function for dumping the JSON content for each button into the div
-      function displayEmojiInfo() {
+  // This function handles events where the GIF button is clicked
+$("add-gif").on("click", function(event){
+  event.preventDefault();
+  // This line grabs the input from the text box
+  let gifRClicked = $("#gif-input").val().trim();
+  // Adding GIF from the textbox to the array
+  gifReactions.push(gifRClicked);
+  // Calling renderButtons which handles the processing of the GIF array
+  renderButtons();
+  });
 
-        let emoji = $(this).attr("data-name");
-        let queryURL = "https://media1.giphy.com/media/CaiVJuZGvR8HK/200_s.gif" + emoji + "&api_key=8wxj1dB7Bhxy1x4Gfpap49uArL6LloD6&limit=5";
+// Add function displayGifInfo to have HTML display content
+function displayGifInfo() {
+  let gifRClicked = $(this).attr("data-name");
+  let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifRClicked + "&api_key=8wxj1dB7Bhxy1x4Gfpap49uArL6LloD6&limit=5";
 
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-          $("#e-view").text(JSON.stringify(response));
-          renderButtons();
-     
+// Create AJAX call for the specific reaction GIF being clicked
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).done(function(response) {
 
-                // Creating a div to hold the movie
-                let eDiv = $("<div class='emoji'>");
 
-                // Storing the rating data
-                let rating = response.rating;
-      
-                // Creating an element to have the rating displayed
-                let pOne = $("<p>").text("Rating: " + rating);
-      
-                // Displaying the rating
-                eDiv.append(pOne);
-      
-                // Retrieving the URL for the image
-                let imgURL = response.images.fixed_height_still.url;
-      
-                // Creating an element to hold the image
-                let image = $("<img>").attr("src", imgURL);
-      
-                // Appending the image
-                eDiv.append(image);
-      
-                // Putting the entire movie above the previous movies
-                $("#e-view").prepend(eDiv);
-              
-      
-            });
-        }
+// Looping through the array of GIFs
+for (let i =0; i < gifReactions.length; i++){
+// Creating a div to hold the GIF
+let gifDiv = $("<div class = 'gifRClicked'>");
+// Store the rating data
+let rating = response[i].rating;
+// Create a <p> element to display the rating data
+let p = $("<p>").text("Rating:" + rating);
+// Display the rating
+gifDiv.append(p); 
+
+// Creating <img> element to hold the image
+let image = $("<img>");
+// Retrieving the URL for the image and giving the <image> an src attribute of a property pulled off the result item
+image.attr("src", results[i].images.fixed_height_still.url);
+image.attr("data-still", results[i].images.fixed_height_still.url);
+image.attr("data-animate", results[i].images.fixed_height.url);            
+// Appending the image
+gifDiv.append(image);
+// Now putting the entire GIF above the previous GIFs
+$("#gif-view").prepend(gifDiv);
+
+}
+});}
+
   
 
-            
-///////////////////////////////////////////////////////
-     
-           // Function for displaying emoji data
-           function renderButtons() {
-     
-             // Deleting the emoji buttons porior to adding new movie buttons
-             // (this is necessary otherwise we will have repeat buttons)
-             $("#e-view").empty();
-     
-             // Looping through the array of emoji topics
-             for (let i = 0; i < topics.length; i++) {
-     
-               // Then dynamicaly generating buttons for each emoji in the array.
-               // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-               let a = $("<button>");
-               // Adding a class of e-btn to our button
-             a.addClass("e-btn");
 
-               // Adding a data-attribute with a value of the emoji at index i
-               a.attr("data-name", topics[i]);
-               // Providing the button's text with a value of the emoji at index i
-               a.text(topics[i]);
-               // Adding the button to the HTML
-               $("#e-view").append(a);
-             }
-           }
-     
-           // This function handles events where one button is clicked
-           $("#add-e").on("click", function(event) {
-             // event.preventDefault() prevents the form from trying to submit itself.
-             // We're using a form so that the user can hit enter instead of clicking the button if they want
-             event.preventDefault();
-     
-             // This line will grab the text from the input box
-             let emoji = $("#e-input").val().trim();
-             // The emoji from the textbox is then added to our array
-             topics.push(emoji);
-     
-             // calling renderButtons which handles the processing of our emoji array
-             renderButtons();
-           });
-     
-            // Adding a click event listener to all elements with a class of "movie-btn"
-      $(document).on("click", ".e-btn", displayEmojiInfo);
 
-      // Calling the renderButtons function to display the intial buttons
-      renderButtons();
+// Adding a click event listener to all elements with class "gif-btn"
+$(document).on("click", ".gif-btn", displayGifInfo)
 
-     
+$(document).on("click", "img", function() {
+  console.log("gif clicked")
+
+  let state = $(this).attr("data-state")
+  console.log(this)
+
+  if (state === "still") {
+
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+
+  } else {
+
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+  }
+});
+
+
+// Calling renderButtons function to display the initial buttons
+renderButtons(); 
